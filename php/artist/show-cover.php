d6e89f5e9a59e825a0469a60228a0bfced66f738
Jenwit_Krit <jenwitkmonz@gmail.com>
2018-04-05 19:42:22 +0530

    Add files via upload

--- php/artist/show-cover.php
+++ php/artist/show-cover.php
@@ -2,12 +2,19 @@
 
     $conn = new mysqli("localhost","root","","project");
     $artist = $_GET['artist'];
-    $data = $conn->query("SELECT * from album where artist = '$artist' GROUP BY artist ");
-    while($row = $data->fetch_assoc()){
-        
-        echo '<img src="data:image/jpeg;base64,'.base64_encode($row['album_cover'] ).'" height="" width="500" class="img-thumnail music-pic" >  ';
 
+    $data_search = $conn->query("SELECT * from music where artist = '$artist' ");
     
+    if($data_search->num_rows > 0){
+        $data = $conn->query("SELECT * from music where artist = '$artist' GROUP BY artist ");
+        while($row = $data->fetch_assoc()){
+            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['music_cover'] ).'" height="" width="500" class="img-thumnail music-pic" >  ';
+        }
+    }else{
+        $data = $conn->query("SELECT * from album where artist = '$artist' GROUP BY artist ");
+        while($row = $data->fetch_assoc()){
+            echo '<img src="data:image/jpeg;base64,'.base64_encode($row['album_cover'] ).'" height="" width="500" class="img-thumnail music-pic" >  ';
+        }
     }
 
 
--- php/artist/show-music.php
+++ php/artist/show-music.php
@@ -2,13 +2,33 @@
 
     $conn = new mysqli("localhost","root","","project");
     $artist=$_GET['artist'];
-    $data = $conn->query("SELECT * from album where  artist = '$artist' ");
-    while($row = $data->fetch_assoc()){
-        $url = $row['album_file'];
-        $music_name = $row['music_name'];
 
-        echo "<li><a href='$url'>$music_name</a></li>";
+    $data_check = $conn->query("SELECT * from music where artist = '$artist' ");
+    $data_count_music = $conn->query("SELECT count(*) from music where artist = '$artist' ");
+    $data_count_album = $conn->query("SELECT count(*) from album where artist = '$artist' ");
 
+    if($data_count_music > $data_count_album){
+        if($data_check->num_rows > 0){
+            $data = $conn->query("SELECT * from music where  artist = '$artist' ");
+            while($row = $data->fetch_assoc()){
+                $url = $row['music_file'];
+                $music_name = $row['music_name'];
+        
+                echo "<li><a href='$url'>$music_name</a></li>";
+        
+            }
+        }
+    }else{
+        $data = $conn->query("SELECT * from album where  artist = '$artist' ");
+
+        if($data->num_rows > 0){
+            while($row = $data->fetch_assoc()){
+                $url = $row['album_file'];
+                $music_name = $row['music_name'];
+        
+                echo "<li><a href='$url'>$music_name</a></li>";
+            }
+        }
     }
 
-?>
+?>        
